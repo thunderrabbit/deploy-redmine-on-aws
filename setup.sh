1fdb1b79e947db9d12670450a78a11bb9b2de65d
Rob Nugen <rob@robnugen.com>
2017-01-29 13:02:20 +0900

    Get redmine-ansible role

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+# get repo that will install redmine; eventually it will be from Ansible Galaxy
+git clone https://bitbucket.org/sbstrm/redmine-ansible.git roles/redmine-ansible
 ./setup-passwords.sh
 ./setup-credentials.sh
