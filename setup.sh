4d8d6f417c2e2b24a905fcaf0d8fb0ebea1b9542
Rob Nugen <rob@robnugen.com>
2017-02-10 21:15:39 +0900

    Install the letsencrypt role

--- setup.sh
+++ setup.sh
@@ -2,5 +2,6 @@
 
 # get repo that will install redmine; eventually it will be from Ansible Galaxy
 git clone https://bitbucket.org/sbstrm/redmine-ansible.git roles/redmine-ansible
+git clone https://bitbucket.org/sbstrm/letsencrypt-ansible.git roles/letsencrypt-ansible
 ./setup-passwords.sh
 ./setup-credentials.sh
